705788bf91a4c61368e23da46c33c944e97841ec
Vinicius <vini383@gmail.com>
2024-01-02 17:33:40 -0300

    feat: added password valueobj

--- JwtStore.Core/Contexts/AccountContext/ValueObjects/Password.cs
+++ JwtStore.Core/Contexts/AccountContext/ValueObjects/Password.cs
@@ -2,7 +2,16 @@
 
 namespace JwtStore.Core.Contexts.AccountContext.ValueObjects
 {
-    public class Password  : ValueObject
+    public class Password : ValueObject
     {
+        //normal chars valid to form a password
+        private const string Valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        //special chars valid to form a password
+        private const string Special = "!@#$%ˆ&*(){}[];Ç";
+
+        public string Hash { get; } = string.Empty;
+        public string ResetCode { get; } = Guid.NewGuid().ToString("N")[..8].ToLower();
+
     }
 }
+ 
